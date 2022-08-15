2860da5c0cf49fb6acec73d4a0abbe87132c2aeb
Jonathan Alvarez <j@jonalvarezz.com>
2022-08-15 14:18:28 +0200

    Typing and using a node ref
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,10 +1,33 @@
+import { useRef, useEffect } from "react";
+
 type Props = {
   image: string;
 };
 
 export function RandomFox({ image }: Props): JSX.Element {
+  const node = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("Intersecting");
+        }
+      });
+    });
+
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <img
+      ref={node}
       width="320"
       height="auto"
       src={image}
